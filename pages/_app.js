c65bf9a4aafacd8e933a00399e9617cfda05ff76
Mikhail8815 <mishutka150588@mail.ru>
2025-07-21 22:44:19 +0300

    Layout & Components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css';
+import Layout from "../components/Layout";
 
 const MyApp = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+    <Layout>
+        <main>
+            <Component {...pageProps} />
+        </main>
+    </Layout>
 );
 
 export default MyApp;
