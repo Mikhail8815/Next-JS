dc74c279bd435d7e80469581089005de51303666
Mikhail8815 <mishutka150588@mail.ru>
2025-07-22 21:52:00 +0300

    Pages Navigation & Redirecting + styles

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,25 @@
-const Error = () => (
-    <>
-        <h1>404</h1>
-        <h2>Something is going wrong...</h2>
-    </>
-);
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Heading from "../components/Heading";
+import styles from "../styles/404.module.scss";
+
+const Error = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000);
+    }, [router]);
+
+    return (
+        <div className={styles.wrapper}>
+            <div>
+                <Heading text="404" />
+                <Heading tag="h2" text="Something is going wrong..." />
+            </div>
+        </div>
+    )
+};
 
 export default Error;
