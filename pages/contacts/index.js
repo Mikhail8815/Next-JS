f702aa91df829732d3287347302d8fc20721f943
Mikhail8815 <mishutka150588@mail.ru>
2025-07-24 19:09:03 +0300

    Dynamic Routes

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Heading from "../../components/Heading";
+import Link from "next/link";
 
 export const getStaticProps = async () => {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -23,8 +24,10 @@ const Contacts = ({contacts}) => (
         </Head>
         <Heading text="Contacts list:"/>
         <ul>
-            {contacts && contacts.map(({id, name, email}) => (
-                <li key={id}><strong>{name}</strong> ({email})</li>
+            {contacts && contacts.map(({id, name}) => (
+                <li key={id}>
+                    <Link href={`/contacts/${id}`}>{name}</Link>
+                </li>
             ))}
         </ul>
     </>
